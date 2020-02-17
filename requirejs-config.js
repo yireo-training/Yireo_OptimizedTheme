d2f6f2138f8cfaa2e0e1390c3fe65a9aa864ba45
Jisse Reitsma <jisse@yireo.com>
2020-02-17 20:49:54 +0100

    Additional removals

--- requirejs-config.js
+++ requirejs-config.js
@@ -7,7 +7,10 @@ var config = {
     paths: {
         'mage/calendar': 'js/zero',
         'Magento_Ui/js/grid/filters/range': 'js/zero',
-        'mage/polyfill': 'js/zero'
+        'mage/polyfill': 'js/zero',
+        //'mage/menu': 'js/zero', // Enable if you have a CSS menu
+        'mage/translate-inline': 'js/zero',
+        'Magento_Captcha/js/model/captcha': 'js/zero',
     },
     skipDataMain: true
 };
